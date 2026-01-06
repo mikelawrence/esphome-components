44fad9965c803719b47644e6986ba6de7efe40b1
Big Mike <mike@bigmike.land>
2026-01-06 15:43:55 -0600

    Massive cleanup Add setup state machine to prevent blocking and allow more time between first command and each subsequent commands. More Command enum to tfmini.cpp allows simpler command names. Data frame logic is a bit tweaked and switched to using helpers. Switch to StaticVector instead of vector. Removed unused functions.

--- components/tfmini/tfmini.cpp
+++ components/tfmini/tfmini.cpp
@@ -6,99 +6,132 @@ namespace esphome {
 namespace tfmini {
 static const char *const TAG = "tfmini";
 
+enum TFminiCmd {
+  CMD_FW_VERSION = 0x01,
+  CMD_SOFT_RESET = 0x02,
+  CMD_SAMPLE_RATE = 0x03,
+  CMD_TRIGGER = 0x04,
+  CMD_OUTPUT_FORMAT = 0x05,
+  CMD_BAUD_RATE = 0x06,
+  CMD_OUTPUT_CONTROL = 0x07,
+  CMD_HARD_RESET = 0x10,
+  CMD_SAVE_SETTINGS = 0x11,
+  CMD_LOW_POWER = 0x35,
+};
+
 static const char *model_to_str(TFminiModel model) {
   switch (model) {
-    case TFminiModel::TFMINI_MODEL_TFMINI_S:
+    case MODEL_TFMINI_S:
       return "TFmini-S";
-    case TFminiModel::TFMINI_MODEL_TFMINI_PLUS:
+    case MODEL_TFMINI_PLUS:
       return "TFmini Plus";
-    case TFminiModel::TFMINI_MODEL_TF_LUNA:
+    case MODEL_TF_LUNA:
       return "TF-Luna";
     default:
-      return "";
+      return "Unknown";
   }
 }
 
 void TFminiComponent::setup() {
-  uint64_t start_time = millis();
-  uint16_t attempts = 0;
-
-  ESP_LOGCONFIG(TAG, "Setting up...");
-
-  // CONFIG must be high for UART comms and low for I2C comms
-  // applies to TF Luna model only
+  // CONFIG must be high for UART comms and low for I2C comms, TF Luna model only
   if (this->config_pin_ != nullptr) {
     this->config_pin_->setup();
     this->config_pin_->digital_write(true);
-    delay(10);
   }
-  if (this->low_power_) {
-    this->sample_rate_received_ = true;         // bypass sending Sample Rate Command
-    this->send_command_(TFMINI_CMD_LOW_POWER);  // send first Low Power Command
-  } else {
-    this->low_power_received_ = true;  // bypass sending Low Power Command
-    if (this->sample_rate_ == 100) {
-      this->sample_rate_received_ = true;  // no need to send default Sample Rate Command
-    } else {
-      this->send_command_(TFMINI_CMD_SAMPLE_RATE);  // send first Set Sample Rate Command
-    }
-  }
+  this->set_timeout(500, [this]() { this->internal_setup_(TFMINI_SM_START); });
+}
 
-  this->send_command_(TFMINI_CMD_FW_VERSION);  // resend Get Firmware Version Command
-
-  attempts = 0;
-
-  while (!this->low_power_received_ || !this->sample_rate_received_ || !this->version_received_) {
-    process_rx_data();
-    if ((millis() - this->cmd_time_) > 100) {
-      // timeout, send command again
+void TFminiComponent::internal_setup_(SetupStates state) {
+  switch (state) {
+    case TFMINI_SM_START:
+      this->cmd_attempts = 0;
+      if (this->low_power_) {
+        this->send_command_(CMD_LOW_POWER);  // send first Low Power Command
+        this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_LOW_POWER); });
+      } else {
+        this->send_command_(CMD_SAMPLE_RATE);  // send first Set Sample Rate Command
+        this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_SAMPLE_RATE); });
+      }
+      break;
+    case TFMINI_SM_LOW_POWER:
       if (!this->low_power_received_) {
-        this->send_command_(TFMINI_CMD_LOW_POWER);  // resend Low Power Command
+        if (++this->cmd_attempts_ < 5) {
+          this->cmd_attempts++;
+          this->send_command_(CMD_LOW_POWER);  // resend Low Power Command
+          ESP_LOGW(TAG, "Resending Low Power Command");
+          this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_LOW_POWER); });
+        } else {
+          // no response, we are in error
+          ESP_LOGE(TAG, ESP_LOG_MSG_COMM_FAIL);
+          this->mark_failed(LOG_STR(ESP_LOG_MSG_COMM_FAIL));
+          break;
+        }
+      } else {
+        this->cmd_attempts = 0;
+        this->send_command_(CMD_SAMPLE_RATE);  // send first Set Sample Rate Command
+        this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_SAMPLE_RATE); });
       }
+      break;
+    case TFMINI_SM_SAMPLE_RATE:
       if (!this->sample_rate_received_) {
-        this->send_command_(TFMINI_CMD_SAMPLE_RATE);  // resend Sample Rate Command
+        if (++this->cmd_attempts_ < 5) {
+          this->cmd_attempts++;
+          this->send_command_(CMD_SAMPLE_RATE);  // resend Sample Rate Command
+          ESP_LOGW(TAG, "Resending Sample Rate Command");
+          this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_SAMPLE_RATE); });
+        } else {
+          // no response, we are in error
+          ESP_LOGE(TAG, ESP_LOG_MSG_COMM_FAIL);
+          this->mark_failed(LOG_STR(ESP_LOG_MSG_COMM_FAIL));
+          break;
+        }
+      } else {
+        this->cmd_attempts = 0;
+        this->send_command_(CMD_FW_VERSION);  // send First Get Firmware Version Command
+        this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_FW_VERSION); });
       }
+      break;
+    case TFMINI_SM_FW_VERSION:
       if (!this->version_received_) {
-        this->send_command_(TFMINI_CMD_FW_VERSION);  // resend Get Firmware Version Command
+        if (++this->cmd_attempts_ < 5) {
+          this->cmd_attempts++;
+          this->send_command_(CMD_FW_VERSION);  // resend Get Firmware Command
+          ESP_LOGW(TAG, "Resending Sample Rate Command");
+          this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_FW_VERSION); });
+        } else {
+          // no response, we are in error
+          ESP_LOGE(TAG, ESP_LOG_MSG_COMM_FAIL);
+          this->mark_failed(LOG_STR(ESP_LOG_MSG_COMM_FAIL));
+          break;
+        }
+      } else {
+        this->cmd_attempts = 0;
+        this->send_command_(CMD_FW_VERSION);  // send First Get Firmware Version Command
+        this->set_timeout(100, [this]() { this->internal_setup_(TFMINI_SM_FW_VERSION); });
       }
-      if (++attempts >= 40) {
-        // ESP_LOGE(TAG, "Low Power=%s, Sample Rate=%s, Version=%s", this->low_power_received_ ? "True" : "False",
-        // this->sample_rate_received_ ? "True" : "False", this->version_received_ ? "True" : "False");
-        ESP_LOGE(TAG, "No response from %s", LOG_STR_ARG(model_to_str(this->model_)));
-        this->mark_failed();
-        break;
-      }
-    }
+      break;
   }
   // received responses from all commands
-  this->setup_ = true;
-  this->config_elapse_ = (millis() - start_time) / 1000.0;
-  ESP_LOGV(TAG, "Config completed in %0.3f sec", this->config_elapse_);
+  this->initialized_ = true;
 }
 
 void TFminiComponent::loop() {
-  if (this->is_failed()) {
-    return;
-  }
   process_rx_data();  // get data from UART
 }
 
 void TFminiComponent::dump_config() {
   ESP_LOGCONFIG(TAG,
-                "TFmini:"
-                "  Model: %s"
-                "  Firmware Version: %s"
-                "  Sample Rate: %u",
-                LOG_STR_ARG(model_to_str(this->model_)), this->firmware_version_.c_str(), this->sample_rate_);
+                "TFmini:\n"
+                "  Model: %s\n"
+                "  Firmware Version: %s\n"
+                "  Sample Rate: %u\n"
+                "  Low Power Mode: %s\n",
+                LOG_STR_ARG(model_to_str(this->model_)), this->firmware_version_.c_str(), this->sample_rate_,
+                TRUEFALSE(this->low_power_));
   LOG_PIN("  CONFIG Pin: ", this->config_pin_);
-  if (this->model_ != TFMINI_MODEL_TFMINI_PLUS)
-    ESP_LOGCONFIG(TAG, "  Low Power Mode: %s", this->low_power_ ? "True" : "False");
-#ifdef USE_SENSOR
   LOG_SENSOR("  ", "Distance:", this->distance_sensor_);
   LOG_SENSOR("  ", "Signal Strength:", this->signal_strength_sensor_);
   LOG_SENSOR("  ", "Temperature:", this->temperature_sensor_);
-#endif
-  ESP_LOGCONFIG(TAG, "  Setup in %0.3f sec", this->config_elapse_);
 }
 
 void TFminiComponent::process_rx_data() {
@@ -126,20 +159,20 @@ void TFminiComponent::process_rx_data() {
         // packet_length only has a few valid values
         if ((packet_length < 5) || (packet_length > 8)) {
           // invalid frame header, clear the buffer
-          if (this->setup_) {
-            ESP_LOGD(TAG, "Communication failed");
+          if (this->initialized_) {
+            ESP_LOGD(TAG, ESP_LOG_MSG_COMM_FAIL);
           }
           this->rx_buffer_.clear();
         } else {
           // valid response frame header
-          if (this->setup_) {
+          if (this->initialized_) {
             ESP_LOGV(TAG, "Start Response Frame Header received");
           }
         }
       } else if ((this->rx_buffer_[0] == 0x59) && (this->rx_buffer_[1] != 0x59)) {
         // invalid start of data frame
-        if (this->setup_) {
-          ESP_LOGD(TAG, "Communication failed");
+        if (this->initialized_) {
+          ESP_LOGD(TAG, ESP_LOG_MSG_COMM_FAIL);
         }
         this->rx_buffer_.clear();
       } else {
@@ -159,12 +192,11 @@ void TFminiComponent::process_rx_data() {
         // processing a 9-byte data frame
         if (this->rx_buffer_.size() == packet_length) {
           // received a 9-byte data frame
-          if (this->setup_) {
+          if (this->initialized_) {
             ESP_LOGV(TAG, "Processing Data Frame");
             this->process_data_frame();
           }
-          // good or bad response, we are done, start over
-          this->rx_buffer_.clear();
+          this->rx_buffer_.clear();  // good or bad response, we are done, start over
         }
       }
     }
@@ -172,30 +204,22 @@ void TFminiComponent::process_rx_data() {
 }
 
 void TFminiComponent::process_data_frame() {
-  float distance;
-  float strength, temperature;
+  float distance, strength, temperature;
 
   if (this->verify_rx_packet_checksum_()) {
-    // checksum is good, grab the three results as little endian 16-bit numbers
-    distance = static_cast<float>((this->rx_buffer_[3] << 8) + this->rx_buffer_[2]);
-    strength = static_cast<float>((this->rx_buffer_[5] << 8) + this->rx_buffer_[4]);
-    temperature = static_cast<float>((this->rx_buffer_[7] << 8) + this->rx_buffer_[6]);
-
-    if (strength < 100) {
-      // not enough signal, most likely open air
-      distance = NAN;  // out of range
-    } else if ((strength == 65535) || (distance == -4)) {
-      // too much signal
-      distance = NAN;  // unable to determine range
+    // checksum is good, results are little endian 16-bit numbers
+    distance = convert_little_endian(*reinterpret_cast<uint16_t *>(&this->rx_buffer_[2]));
+    strength = convert_little_endian(*reinterpret_cast<uint16_t *>(&this->rx_buffer_[4]));
+    if ((strength < 100) || (strength == 65535) || (distance == -4)) {
+      distance = NAN;  // distance is invalid under these conditions
     }
-    if (this->distance_sensor_ != nullptr)
-      this->distance_sensor_->publish_state(float(distance));
-
+    this->distance_sensor_->publish_state(static_cast<float>(distance));
     if (this->signal_strength_sensor_ != nullptr) {
-      this->signal_strength_sensor_->publish_state(float(strength));
+      this->signal_strength_sensor_->publish_state(static_cast<float>(strength));
     }
     if (this->temperature_sensor_ != nullptr) {
-      this->temperature_sensor_->publish_state((float(temperature) / 8.0) - 256.0);
+      temperature = convert_little_endian(*reinterpret_cast<uint16_t *>(&this->rx_buffer_[6]));
+      this->temperature_sensor_->publish_state((static_cast<float>(temperature) / 8.0) - 256.0);
     }
   }
 }
@@ -203,34 +227,31 @@ void TFminiComponent::process_data_frame() {
 void TFminiComponent::process_response_frame() {
   uint16_t value;
 
-  if (this->rx_buffer_[2] == TFMINI_CMD_FW_VERSION) {
+  if (this->rx_buffer_[2] == CMD_FW_VERSION) {
     if (this->verify_rx_packet_checksum_()) {
       this->firmware_version_ = 'v' + std::to_string(this->rx_buffer_[5]) + '.' + std::to_string(this->rx_buffer_[4]) +
                                 '.' + std::to_string(this->rx_buffer_[3]);
       this->version_received_ = true;
       ESP_LOGV(TAG, "Received Firmware Version Response Frame, Version = %s", this->firmware_version_.c_str());
     }
-  } else if (this->rx_buffer_[2] == TFMINI_CMD_SOFT_RESET) {
+  } else if (this->rx_buffer_[2] == CMD_SOFT_RESET) {
     if (this->verify_rx_packet_checksum_()) {
-      // checksums match
       this->soft_reset_received_ = true;
       ESP_LOGV(TAG, "Received Soft Reset Response Frame, Soft Reset was %s",
                this->rx_buffer_[3] == 0 ? "successful" : "failed");
     }
-  } else if (this->rx_buffer_[2] == TFMINI_CMD_SAMPLE_RATE) {
+  } else if (this->rx_buffer_[2] == CMD_SAMPLE_RATE) {
     if (this->verify_rx_packet_checksum_()) {
-      // checksums match
       value = (this->rx_buffer_[4] << 8) + this->rx_buffer_[3];
       if (this->sample_rate_ == value) {
         this->sample_rate_received_ = true;
         ESP_LOGV(TAG, "Received Sample Rate Response Frame, match");
       } else {
-        ESP_LOGE(TAG, "Command Error", value);
+        ESP_LOGE(TAG, "Command Response Error", value);
       }
     }
-  } else if (this->rx_buffer_[2] == TFMINI_CMD_LOW_POWER) {
+  } else if (this->rx_buffer_[2] == CMD_LOW_POWER) {
     if (this->verify_rx_packet_checksum_()) {
-      // checksums match
       value = this->low_power_;
       if (this->sample_rate_ > 10) {
         value = 10;
@@ -242,12 +263,11 @@ void TFminiComponent::process_response_frame() {
         this->low_power_received_ = true;
         ESP_LOGV(TAG, "Received Low Power Response Frame, match");
       } else {
-        ESP_LOGD(TAG, "Command Error");
+        ESP_LOGD(TAG, "Command Response Error");
       }
     }
   } else {
-    // not a valid response command, clear the buffer
-    ESP_LOGD(TAG, "Communication failed");
+    ESP_LOGD(TAG, ESP_LOG_MSG_COMM_FAIL);
   }
 }
 
@@ -256,45 +276,45 @@ void TFminiComponent::send_command_(uint8_t cmd) {
 
   this->tx_buffer_.clear();
   switch (cmd) {
-    case TFMINI_CMD_FW_VERSION:
+    case CMD_FW_VERSION:
       this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
       this->tx_buffer_.push_back(0x04);  // This command is 4 bytes long
-      this->tx_buffer_.push_back(TFMINI_CMD_FW_VERSION);
+      this->tx_buffer_.push_back(CMD_FW_VERSION);
       this->comp_cs_send_command_();
       this->version_received_ = false;
-      ESP_LOGV(TAG, "Sent Get Firmware Version command. %s", this->tx_packet_to_str_().c_str());
+      ESP_LOGV(TAG, "Sent Get Firmware Version command.");
       break;
 
-    case TFMINI_CMD_SOFT_RESET:
-      this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
-      this->tx_buffer_.push_back(0x04);  // This command is 4 bytes long
-      this->tx_buffer_.push_back(TFMINI_CMD_SOFT_RESET);
-      this->comp_cs_send_command_();
-      this->soft_reset_received_ = false;
-      ESP_LOGV(TAG, "Sent Soft Reset command. %s", this->tx_packet_to_str_().c_str());
-      break;
+    // case CMD_SOFT_RESET:
+    //   this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
+    //   this->tx_buffer_.push_back(0x04);  // This command is 4 bytes long
+    //   this->tx_buffer_.push_back(CMD_SOFT_RESET);
+    //   this->comp_cs_send_command_();
+    //   this->soft_reset_received_ = false;
+    //   ESP_LOGV(TAG, "Sent Soft Reset command.");
+    //   break;
 
-    case TFMINI_CMD_SAMPLE_RATE:
+    case CMD_SAMPLE_RATE:
       value = this->sample_rate_;
       this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
       this->tx_buffer_.push_back(0x06);  // This command is 6 bytes long
-      this->tx_buffer_.push_back(TFMINI_CMD_SAMPLE_RATE);
+      this->tx_buffer_.push_back(CMD_SAMPLE_RATE);
       this->tx_buffer_.push_back(value & 0xFF);         // Sample Rate low byte
       this->tx_buffer_.push_back((value >> 8) & 0xFF);  // Sample Rate high byte
       this->comp_cs_send_command_();
       this->sample_rate_received_ = false;
-      ESP_LOGV(TAG, "Sent Set Sample Rate command. Sample Rate = %u. %s", value, this->tx_packet_to_str_().c_str());
+      ESP_LOGV(TAG, "Sent Set Sample Rate command. Sample Rate = %u.", value);
       break;
 
-    case TFMINI_CMD_TRIGGER:
-      this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
-      this->tx_buffer_.push_back(0x04);  // This command is 4 bytes long
-      this->tx_buffer_.push_back(TFMINI_CMD_TRIGGER);
-      this->comp_cs_send_command_();
-      ESP_LOGV(TAG, "Sent Trigger Detection command. %s", this->tx_packet_to_str_().c_str());
-      break;
+    // case CMD_TRIGGER:
+    //   this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
+    //   this->tx_buffer_.push_back(0x04);  // This command is 4 bytes long
+    //   this->tx_buffer_.push_back(CMD_TRIGGER);
+    //   this->comp_cs_send_command_();
+    //   ESP_LOGV(TAG, "Sent Trigger Detection command.");
+    //   break;
 
-    case TFMINI_CMD_LOW_POWER:
+    case CMD_LOW_POWER:
       value = this->sample_rate_;
       if (this->sample_rate_ > 10)
         value = 10;
@@ -302,16 +322,14 @@ void TFminiComponent::send_command_(uint8_t cmd) {
         value = 0;
       this->tx_buffer_.push_back(0x5A);  // Commands always starts with 0x5A
       this->tx_buffer_.push_back(0x06);  // This command is 6 bytes long
-      this->tx_buffer_.push_back(TFMINI_CMD_LOW_POWER);
+      this->tx_buffer_.push_back(CMD_LOW_POWER);
       this->tx_buffer_.push_back(value);  // Sample Rate low byte
       this->tx_buffer_.push_back(0x00);   // always 0x00
       this->comp_cs_send_command_();
       this->low_power_received_ = false;
-      ESP_LOGV(TAG, "Sent Low Power command. Low Power Mode = %s. %s", this->low_power_ ? "Enabled" : "Disabled",
-               this->tx_packet_to_str_().c_str());
+      ESP_LOGV(TAG, "Sent Low Power command. Low Power Mode = %s.", this->low_power_ ? "Enabled" : "Disabled");
       break;
   }
-  this->cmd_time_ = millis();
 }
 
 void TFminiComponent::comp_cs_send_command_() {
@@ -336,30 +354,8 @@ bool TFminiComponent::verify_rx_packet_checksum_() {
   if (checksum == this->rx_buffer_[length - 1]) {
     return true;
   }
-  ESP_LOGD(TAG, "Communication failed");
+  ESP_LOGD(TAG, ESP_LOG_MSG_COMM_FAIL);
   return false;
-}
-
-std::string TFminiComponent::rx_packet_to_str_() {
-  uint8_t length = this->rx_buffer_.size();
-  std::string packet_str = "Packet <- ";
-
-  for (uint8_t i = 0; i < length - 1; i++) {
-    packet_str += format_hex(this->rx_buffer_[i]) + ' ';
-  }
-  packet_str += format_hex(this->rx_buffer_[length - 1]);
-  return packet_str;
-}
-
-std::string TFminiComponent::tx_packet_to_str_() {
-  uint8_t length = this->tx_buffer_.size();
-  std::string packet_str = "Packet -> ";
-
-  for (uint8_t i = 0; i < length - 1; i++) {
-    packet_str += format_hex(this->tx_buffer_[i]) + ' ';
-  }
-  packet_str += format_hex(this->tx_buffer_[length - 1]);
-  return packet_str;
 }
 
 }  // namespace tfmini
--- components/tfmini/tfmini.h
+++ components/tfmini/tfmini.h
@@ -8,82 +8,52 @@
 
 namespace esphome {
 namespace tfmini {
-enum TFminiModel {
-  TFMINI_MODEL_TFMINI_S = 0,
-  TFMINI_MODEL_TFMINI_PLUS,
-  TFMINI_MODEL_TF_LUNA,
-};
-
-enum TFminiCmd {
-  TFMINI_CMD_FW_VERSION = 0x01,
-  TFMINI_CMD_SOFT_RESET = 0x02,
-  TFMINI_CMD_SAMPLE_RATE = 0x03,
-  TFMINI_CMD_TRIGGER = 0x04,
-  TFMINI_CMD_OUTPUT_FORMAT = 0x05,
-  TFMINI_CMD_BAUD_RATE = 0x06,
-  TFMINI_CMD_OUTPUT_CONTROL = 0x07,
-  TFMINI_CMD_HARD_RESET = 0x10,
-  TFMINI_CMD_SAVE_SETTINGS = 0x11,
-  TFMINI_CMD_LOW_POWER = 0x35,
+enum TFminiModel { MODEL_TFMINI_S = 0, MODEL_TFMINI_PLUS, MODEL_TF_LUNA };
+enum SetupStates {
+  TFMINI_SM_START,
+  TFMINI_SM_LOW_POWER,
+  TFMINI_SM_SAMPLE_RATE,
+  TFMINI_SM_FW_VERSION,
+  TFMINI_SM_GET_FW
 };
 
 class TFminiComponent : public uart::UARTDevice, public Component {
- public:
-#ifdef USE_SENSOR
   SUB_SENSOR(distance)
   SUB_SENSOR(signal_strength)
   SUB_SENSOR(temperature)
-#endif
 
-  void set_model(TFminiModel model) { this->model_ = model; };
-  // void set_distance_sensor(sensor::Sensor *distance_sensor) { this->distance_sensor_ = distance_sensor; };
-  void set_sample_rate(uint32_t rate) { this->sample_rate_ = rate; };
-  void set_config_pin(GPIOPin *pin) { this->config_pin_ = pin; }
-  // void set_signal_strength_sensor(sensor::Sensor *signal_strength_sensor) {
-  //   this->signal_strength_sensor_ = signal_strength_sensor;
-  // };
-  // void set_temperature_sensor(sensor::Sensor *temperature_sensor) { this->temperature_sensor_ = temperature_sensor; };
-  void set_low_power_mode(uint32_t mode) { this->low_power_ = mode; };
-
-  // ========== INTERNAL METHODS ==========
+ public:
   void setup() override;
   void loop() override;
   void dump_config() override;
+  void set_model(TFminiModel model) { this->model_ = model; };
+  void set_sample_rate(uint32_t rate) { this->sample_rate_ = rate; };
+  void set_config_pin(GPIOPin *pin) { this->config_pin_ = pin; }
+  void set_low_power_mode(uint32_t mode) { this->low_power_ = mode; };
 
  protected:
-  bool setup_{false};
-  bool error_{false};
-  uint64_t cmd_time_{0};
-  float config_elapse_{0};
-
-  // Config Variables
-  TFminiModel model_;
-  uint32_t sample_rate_{100};
-  // uint16_t dir_sample_num_{10};
-  bool low_power_{false};
-
-  // Local Variables
-  bool soft_reset_received_ = false;
-  bool sample_rate_received_ = false;
-  bool low_power_received_ = false;
-  bool version_received_ = false;
-  std::string firmware_version_{"v0.0.0"};
-
-  std::vector<uint8_t> rx_buffer_;
-  std::vector<uint8_t> tx_buffer_;
-
-  // Methods
+  void internal_setup_(SetupStates state);
   bool verify_rx_packet_checksum_();
-  std::string rx_packet_to_str_();
-  std::string tx_packet_to_str_();
   void process_rx_data();
   void process_data_frame();
   void process_response_frame();
   void send_command_(uint8_t cmd);
   void comp_cs_send_command_();
-
-  // Pins
+  std::string rx_packet_to_str_();
+  std::string tx_packet_to_str_();
+  bool initialized_{false};
+  bool low_power_{false};
+  bool soft_reset_received_{false};
+  bool sample_rate_received_{false};
+  bool low_power_received_{false};
+  bool version_received_{false};
+  uint16_t cmd_attempts_;
+  uint32_t sample_rate_{100};
+  TFminiModel model_;
   GPIOPin *config_pin_{nullptr};
+  std::string firmware_version_{"Unknown"};
+  StaticVector<uint8_t, 10> rx_buffer_;
+  StaticVector<uint8_t, 10> tx_buffer_;
 };
 
 }  // namespace tfmini
